c96a668aa713a1008a3aad2f1031998829b99b97
codeangler <burnett.casey@gmail.com>
2016-06-08 14:20:05 -0600

    simple mad lib

--- app.js
+++ app.js
@@ -5,35 +5,61 @@ angular.module('MadLibApp', [])
   
   function madCtrl($http) {
     var madCtrl = this;
-    madCtrl.greeting = "hi";
 
-    madCtrl.person = function() {
-      var ranPerson = Math.floor(Math.random()*87)
-      $http.get('http://swapi.co/api/people/' + ranPerson)
-       .then(function(response){
-          console.log("inside the random $http " + response.data.name)
-          madCtrl.printPerson = response.data.name;
-        }, function(error) {
-            console.error(error)
-        })
+// ####################  function generate random persons
+    madCtrl.peopleArr = [];
+    madCtrl.person = function(i) {
+      while ( i > 0){
+        var ranPerson = Math.floor(Math.random()*87)
+        $http.get('http://swapi.co/api/people/' + ranPerson)
+         .then(function(response){
+            madCtrl.space = madCtrl.peopleArr.push(response.data.name);
+            
+          }, function(error) {
+              console.error(error)
+          })
 
+         i--;
+      }
     }
-    madCtrl.person()
+    madCtrl.person(3)
+    console.log(madCtrl.peopleArr)
 
-    // var ranStarship = Math.floor(Math.random()*61)
-    //  $http.get('http://swapi.co/api/planets/' + ranStarship)
-    //   .then(function(response){
-    //       console.log(response.data.name)
-    //     }, function(error) {
-    //         console.error(error)
-    //     })
+// ####################  function generate random starship
+    madCtrl.shipArr = [];
+    madCtrl.ship = function(i) {
+      while ( i > 0){
+        var ranShip = Math.floor(Math.random()*37)
+        $http.get('http://swapi.co/api/starships/' + ranShip)
+         .then(function(response){
+            madCtrl.space = madCtrl.shipArr.push(response.data.name);
+          }, function(error) {
+              console.error(error)
+          })
 
-    // var ranPlanet = Math.floor(Math.random()*61);
-    // $http.get('http://swapi.co/api/planets/' + ranPlanet)
-    //   .then(function(response){
-    //       console.log(response.data.name)
-    //     }, function(error) {
-    //         console.error(error)
-    //     })
+         i--;
+      }
+    }
+    madCtrl.ship(1)
+    console.log(madCtrl.shipArr)
+
+// ####################  function generate random planet
+    madCtrl.planetArr = [];
+    madCtrl.planet = function(i) {
+      while ( i > 0){
+        var ranPlanet = Math.floor(Math.random()*22)
+        $http.get('http://swapi.co/api/planets/' + ranPlanet)
+         .then(function(response){
+            madCtrl.space = madCtrl.planetArr.push(response.data.name);
+            
+          }, function(error) {
+              console.error(error);
+          })
+
+         i--;
+      }
+    }
+    madCtrl.planet(3)
+    console.log(madCtrl.planetArr)
 
   }
