7e0e30cb4b2c30630813531f1f1ff4b2a584a5c5
codeangler <burnett.casey@gmail.com>
2016-06-08 14:35:34 -0600

    push index for whole api

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ angular.module('MadLibApp', [])
     var madCtrl = this;
 
 // ####################  function generate random persons
+    var ranPerson = Math.floor(Math.random()*87)
     madCtrl.peopleArr = [];
     madCtrl.person = function(i) {
       while ( i > 0){
-        var ranPerson = Math.floor(Math.random()*87)
-        $http.get('http://swapi.co/api/people/' + ranPerson)
+        $http.get('http://swapi.co/api/people/' + i)
          .then(function(response){
             madCtrl.space = madCtrl.peopleArr.push(response.data.name);
-            
+                console.log(madCtrl.peopleArr)
+
           }, function(error) {
               console.error(error)
           })
@@ -22,15 +23,13 @@ angular.module('MadLibApp', [])
          i--;
       }
     }
-    madCtrl.person(3)
-    console.log(madCtrl.peopleArr)
-
+    madCtrl.person(87)
 // ####################  function generate random starship
+    var ranShip = Math.floor(Math.random()*37)
     madCtrl.shipArr = [];
     madCtrl.ship = function(i) {
       while ( i > 0){
-        var ranShip = Math.floor(Math.random()*37)
-        $http.get('http://swapi.co/api/starships/' + ranShip)
+        $http.get('http://swapi.co/api/starships/' + i)
          .then(function(response){
             madCtrl.space = madCtrl.shipArr.push(response.data.name);
           }, function(error) {
@@ -40,26 +39,27 @@ angular.module('MadLibApp', [])
          i--;
       }
     }
-    madCtrl.ship(1)
-    console.log(madCtrl.shipArr)
+    madCtrl.ship(37)
+    
 
 // ####################  function generate random planet
+    // var ranPlanet = Math.floor(Math.random()*37)
     madCtrl.planetArr = [];
     madCtrl.planet = function(i) {
       while ( i > 0){
-        var ranPlanet = Math.floor(Math.random()*22)
-        $http.get('http://swapi.co/api/planets/' + ranPlanet)
+        $http.get('http://swapi.co/api/planets/' + i)
          .then(function(response){
             madCtrl.space = madCtrl.planetArr.push(response.data.name);
-            
+            console.log(madCtrl.planetArr)
+
           }, function(error) {
               console.error(error);
           })
+        i--;
 
-         i--;
       }
-    }
-    madCtrl.planet(3)
-    console.log(madCtrl.planetArr)
 
+    }
+    madCtrl.planet(37)
+    
   }
